5bef5d19b2aed3cbe10918e1624e1f72b5614ae1
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-11-09 08:25:34 +0000

    node set-up, installed express ejs body-parser mongoose. Added GET, POST, DELETE requests

--- app.js
+++ app.js
@@ -25,7 +25,58 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-//TODO
+app
+  .route("/articles")
+
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully deleted");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+app
+  .route("/articles/:articleTitle")
+
+  .get(function (req, res) {
+    Article.findOne(
+      { title: req.params.articleTitle },
+      function (err, foundArticle) {
+        if (foundArticle) {
+          res.send(foundArticle);
+        } else {
+          res.send("No articles matching that title was found");
+        }
+      }
+    );
+  });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
